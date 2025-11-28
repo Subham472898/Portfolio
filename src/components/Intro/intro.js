e5ffdcc08c494eea6ab7a7b37d3774544be64052
Subham472898 <subhamsurajeetbarik@gmail.com>
2025-11-28 21:35:14 +0530

    Update intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,22 +3,20 @@ import './intro.css';
 import profileImg from '../../assets/image.png';
 
 const Intro = () => (
-  <section id="intro">
-    <img src={profileImg} alt="Profile" className="intro-img" />
-    <h1 className="intro-title">
-      Hello, I'm <span className="introName">Subham</span>
-    </h1>
-    <p className="intro-desc">
-      A passionate software developer skilled in full-stack web development, cloud technology, and problem solving. I love building modern, scalable applications and turning ideas into reality.
-    </p>
-    <a
-      className="intro-btn"
-      href={process.env.PUBLIC_URL + '/resume.pdf'}
-      download="resume.pdf"
-    >
-      Download Resume
-    </a>
-  </section>
+    <section id="intro">
+        <img src={profileImg} alt="Profile" className="intro-img" />
+        <h1 className="intro-title">
+          Hello, I'm <span className="introName">Subham</span>
+        </h1>
+        <p className="intro-desc">A passionate software developer skilled in full-stack web development, cloud technology, and problem solving. I love building modern, scalable applications and turning ideas into reality.</p>
+        <a
+          className="intro-btn"
+          href={process.env.PUBLIC_URL + '/resume.pdf'}
+          download="resume.pdf"
+        >
+          Download Resume
+        </a>
+    </section>
 );
 
 export default Intro;
